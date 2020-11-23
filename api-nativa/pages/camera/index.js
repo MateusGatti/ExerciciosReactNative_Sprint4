c635f21ea7fb53fec87e4f604d44b8682bf13979
Mateus Mello <mt.gatti@hotmail.com>
2020-11-23 15:05:30 -0300

    Camera ajustada

--- api-nativa/pages/camera/index.js
+++ api-nativa/pages/camera/index.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, TouchableOpacity, Modal, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, Image, Button } from 'react-native';
 import { Camera } from 'expo-camera';
-import {FontAwesome} from '@expo/vector-icons';
 
-export default function Foto() {
+const Foto = () => {
 
-    const camRef = useRef(null);
     const [hasPermission, setHasPermission] = useState(null);
+
     const [type, setType] = useState(Camera.Constants.Type.back);
-    const [capturedPhoto, setCapturedPhoto] = useState(null);
-    const [open, setOpen] = useState(false);
 
+    const[imagemUri, setImagemUri] = useState(null);
 
+    // Pedindo permissão do usuario para usar a camera
     useEffect(() => {
         (async () => {
             const { status } = await Camera.requestPermissionsAsync();
@@ -19,24 +18,32 @@ export default function Foto() {
         })();
     }, []);
 
+    const tirarFoto = async () => {
+        if(camera){
+            let foto = await camera.takePictureAsync();
+            setImagemUri(foto.uri);
+            alert('Foto tirada');
+        }
+        
+
+    }
+
     if (hasPermission === null) {
         return <View />;
     }
     if (hasPermission === false) {
-        return <Text>Acesso Negado</Text>;
-    }
-
-    async function takePicture() {
-        if (camRef) {
-            const data = await camRef.current.takePictureAsync();
-            setCapturedPhoto(data.uri);
-            setOpen(true);
-            console.log(data);
-        }
+        return <Text>Sem permissão para usar a câmera</Text>;
     }
     return (
         <View style={{ flex: 1 }}>
-            <Camera style={{ flex: 1 }} type={type} ref={camRef}>
+            <Camera 
+                style={{ flex: 1 }} 
+                type={type}
+                ref={ref => {
+                    camera = ref;
+                }}
+                >
+
                 <View
                     style={{
                         flex: 1,
@@ -56,35 +63,18 @@ export default function Foto() {
                                     : Camera.Constants.Type.back
                             );
                         }}>
-                        <Text style={{ fontSize: 16, marginBottom: 12, marginLeft: 10, color: 'white' }}> Flip </Text>
+                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
                     </TouchableOpacity>
-
-                    {capturedPhoto &&
-                        <Modal
-                            animationType="slide"
-                            transparent={false}
-                            visible={open}>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 20 }}>
-                                <TouchableOpacity style={{ margin: 10 }} onPress={() => setOpen(false)}>
-                                    <FontAwesome name="window-close" size={50} color="blue" />
-                                </TouchableOpacity>
-
-                                <Image
-                                    style={{ width: '100%', height: 300, borderRadius: 20 }}
-                                    source={{ uri: capturedPhoto }}
-                                />
-
-                            </View>
-
-
-                        </Modal>
-                    }
                 </View>
             </Camera>
 
-            <TouchableOpacity onPress={takePicture}>
-                <Text name="camera" size={23} style={{ textAlign: 'center', color: 'black'}}>Tirar foto</Text>
-            </TouchableOpacity>
+            <Image source={{ uri : imagemUri }} style={{ height : 220 }} />
+            <Button title="Tirar foto" onPress={() => tirarFoto()} />
+
+
         </View>
     );
+
 }
+
+export default Foto
